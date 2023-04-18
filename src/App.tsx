9a82c3376394f00e9e94fab773609ee7efd031eb
masoud <masoud30376@gmail.com>
2023-04-18 10:28:48 +0430

    add logic to InputField component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React from "react";
 import "./App.css";
+import React from "react";
+import { useState } from "react";
 import InputField from "./components/InputField";
+import { Todo } from "./Model";
 
 const App: React.FC = () => {
+	const [todo, setTodo] = useState<string>("");
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	const handleAdd = (e: React.FormEvent) => {
+		e.preventDefault();
+		console.log(todo);
+		if (todo) {
+			setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+			setTodo("");
+		}
+	};
+
+	console.log(todos);
+
 	return (
 		<div className="w-full h-[100vh] flex flex-col items-center bg-[#2f74c0] font-Neu">
 			<header className="uppercase text-white text-[40px] my-[30px] z-10 ">
 				Taskify
 			</header>
-			<InputField />
+			<InputField todo={todo} setTodo={setTodo} addHandler={handleAdd} />
 		</div>
 	);
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 
-const InputField = () => {
+interface IProps {
+	todo: string;
+	setTodo: React.Dispatch<React.SetStateAction<string>>;
+	addHandler: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<IProps> = ({ todo, setTodo, addHandler }) => {
 	return (
-		<form className="flex w-[90%] relative items-center">
+		<form
+			className="flex w-[90%] relative items-center"
+			onSubmit={addHandler}
+		>
 			<input
 				type="text"
+				value={todo}
+				onChange={(e) => setTodo(e.target.value)}
 				placeholder="enter task"
 				className="capitalize w-full rounded-[50px] py-[20px] px-[30px] text-2xl duration-[2s] shadow-[inset_0_0_5px_rgba(0,0,0)] focus:shadow-[0_0_10px_1000px_rgba(0,0,0,.5)] focus:outline-none"
 			/>
